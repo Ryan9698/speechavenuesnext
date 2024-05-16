d5832b55501f7c640034c9997d576e0074e32c38
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-05-16 17:31:27 -0400

    Push with coding edits

--- speechavenues/src/app/forms/page.jsx
+++ speechavenues/src/app/forms/page.jsx
@@ -1,7 +1,20 @@
-export default function FormPage() {
+// Forms.jsx
+
+import React from "react";
+import PacketList from "@/components/PacketList";
+
+export default function Forms() {
   return (
-    <main>
-      <div>Hello this is a test(Forms)</div>
-    </main>
+    <div className="container mx-auto p-4">
+      <div>
+        <h1 className="text-4xl font-bold mb-6 cursiveFont">Forms</h1>
+      </div>
+      <p className="mt-4">
+        Below you will find downloadable intake forms for our services. We ask
+        that you please print out these forms and fill them out before you
+        arrive.
+      </p>
+      <PacketList />
+    </div>
   );
 }
