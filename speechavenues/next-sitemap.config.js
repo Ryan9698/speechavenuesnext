92d7d4e18b1574c30149f407935d14b672487993
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-07-29 13:57:37 -0400

    Added next-sitemap and config for robots.txt

---
+++ speechavenues/next-sitemap.config.js
@@ -0,0 +1,17 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.SITE_URL || "https://speechavenues.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false, // Not needed for this site at this point
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "*", disallow: "/forms" },
+      { userAgent: "*", disallow: "/gallery" },
+    ],
+  },
+};
+
+module.exports = config;
