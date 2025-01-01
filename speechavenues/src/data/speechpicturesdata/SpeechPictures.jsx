747c4bb7d294e96867840828b25b1480c9241be7
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-12-31 22:34:46 -0500

    Updated photos for Speech page with better quality

--- speechavenues/src/data/speechpicturesdata/SpeechPictures.jsx
+++ speechavenues/src/data/speechpicturesdata/SpeechPictures.jsx
@@ -1,23 +1,28 @@
 const speechSlides = [
   {
     original:
-      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1716066102/Speech%20Avenues/Speech%20Therapy/large/speech2_sefoik.png',
-    originalAlt: 'Speech Therapy Image 1',
+      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1735691356/Speech%20Avenues/Speech%20Therapy/speech6_wxpxjk.webp',
+    originalAlt: 'Speech Therapy Image 1 AAC Device',
   },
   {
     original:
-      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1716066101/Speech%20Avenues/Speech%20Therapy/large/speech4_ulqhdm.png',
-    originalAlt: 'Speech Therapy Image 2',
+      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1735691149/Speech%20Avenues/Speech%20Therapy/speech2_sefoik_vcx8xb.webp',
+    originalAlt: 'Speech Therapy Image 2 Shapes',
   },
   {
     original:
-      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1716066100/Speech%20Avenues/Speech%20Therapy/large/speech3_xuswat.png',
+      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1735691149/Speech%20Avenues/Speech%20Therapy/speech1_vpkqru_apbyqw.webp',
     originalAlt: 'Speech Therapy Image 3',
   },
   {
     original:
-      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1716066099/Speech%20Avenues/Speech%20Therapy/large/speech1_vpkqru.png',
+      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1735691355/Speech%20Avenues/Speech%20Therapy/speech5_dhj5wh.webp',
     originalAlt: 'Speech Therapy Image 4',
+  },
+  {
+    original:
+      'https://res.cloudinary.com/dfchcqbiq/image/upload/v1735691148/Speech%20Avenues/Speech%20Therapy/speech3_xuswat_w4ckum.webp',
+    originalAlt: 'Speech Therapy Image 5',
   },
 ];
 
